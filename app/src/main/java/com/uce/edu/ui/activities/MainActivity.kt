cbd10ae00cd34d03ed268f7db1028d567016b9f5
Cristian <cdavidq.22@gmail.com>
2023-06-05 22:48:39 -0500

    agregando recursos

--- app/src/main/java/com/uce/edu/ui/activities/MainActivity.kt
+++ app/src/main/java/com/uce/edu/ui/activities/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.uce.edu.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import com.uce.edu.R
+import com.uce.edu.databinding.ActivityMainBinding
+import com.uce.edu.logic.validator.LoginValidator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart(){
+        super.onStart()
+        initClass()
+    }
+
+    fun initClass() {
+        var btnIng = binding.btnIngresar.setOnClickListener {
+            val check = LoginValidator().checkLogin(
+                binding.txtOrreo.text.toString(),
+                binding.txtPass.text.toString()
+            )
+            if (check){
+                var intent = Intent(this,SegundaPantalla::class.java)
+
+                intent.putExtra("var1","")
+                intent.putExtra("var2",2)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
--- app/src/main/java/com/uce/edu/ui/activities/SegundaPantalla.kt
+++ app/src/main/java/com/uce/edu/ui/activities/SegundaPantalla.kt
@@ -1,19 +1,35 @@
 package com.uce.edu.ui.activities
 
-import android.app.Activity
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.uce.edu.R
-
+import com.uce.edu.databinding.ActivitySegundaPantallaBinding
 class SegundaPantalla : AppCompatActivity() {
 
-    //private lateinit var binding:
+    private lateinit var binding:ActivitySegundaPantallaBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda_pantalla)
-
+        binding = ActivitySegundaPantallaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
+    override fun onStart() {
+        super.onStart()
+        initClas()
+    }
+
+    fun initClas(){
+
+        var btnVol = binding.btnVolver.setOnClickListener {
+            var intent = Intent(this,MainActivity::class.java)
+
+            startActivity(intent)
+        }
+
+    }
 
 }
